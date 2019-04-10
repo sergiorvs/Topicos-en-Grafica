da0920f3bff6f89049e5386c3b1008a0f7693308
sergiorvs <segio.rolando@gmail.com>
2019-04-10 17:15:58 -0500

    Add files via upload

--- practica2.py
+++ practica2.py
@@ -31,6 +31,7 @@ def drawHistogram(image_path):
         to_add = sk*255
         sks.append(int(round(to_add, 0)))
     
+    print(sks)
     plt.plot(sks, color = 'red')
     plt.show()
 
@@ -61,6 +62,22 @@ def media(image_path):
                                int(img[i-1][j+1]) + int(img[i][j+1]) + int(img[i+1][j+1]))/9
     
     cv2.imshow('Media', new_image)
+    cv2.waitKey()
+
+def media_ponderada(image_path, peso):
+    img = cv2.imread(image_path, 0)
+    rows, cols = img.shape[:2]
+    new_image = img.copy()
+
+    cv2.imshow('Original', img)
+
+    for i in range(1, rows-1):
+        for j in range(1, cols-1):
+            new_image[i][j] = (int(img[i-1][j-1]) + int(img[i][j-1]) + int(img[i+1][j-1]) + 
+                               int(img[i-1][j]) + peso*int(img[i][j]) + int(img[i+1][j]) + 
+                               int(img[i-1][j+1]) + int(img[i][j+1]) + int(img[i+1][j+1]))/(8+peso)
+    
+    cv2.imshow('MediaPonderada', new_image)
     cv2.waitKey()
 
 def mediana(image_path):
@@ -203,27 +220,91 @@ def laPlace(image_path):
     cv2.imshow('laPlace', new_image)
     cv2.waitKey()     
 
+def roberts(image_path):
+    img = cv2.imread(image_path, 0)
+    rows, cols = img.shape[:2]
+
+    Gx = img.copy()
+    Gy = img.copy()
+
+    cv2.imshow('Original', img)
+
+    for i in range(1, rows-1):
+        for j in range(1, cols-1):
+            Gx[i][j] = -1*int(img[i-1][j-1]) + int(img[i][j])
+            Gy[i][j] = -1*int(img[i-1][j]) + int(img[i][j-1])
+    
+    new_image = Gx + Gy 
+    cv2.imshow('Gx', Gx)
+    cv2.imshow('Gy', Gy)
+    cv2.imshow('Roberts', new_image)
+    cv2.waitKey()     
+        
+def sobel(image_path):
+    img = cv2.imread(image_path, 0)
+    rows, cols = img.shape[:2]
+
+    Gx = img.copy()
+    Gy = img.copy()
+
+    cv2.imshow('Original', img)
+
+    for i in range(1, rows-1):
+        for j in range(1, cols-1):
+            Gx[i][j] = int(img[i-1][j-1]) - int(img[i-1][j+1]) + 2*int(img[i][j-1]) - 2*int(img[i][j+1]) + int(img[i+1][j-1]) - int(img[i+1][j+1])
+            Gy[i][j] = int(img[i-1][j-1]) - int(img[i+1][j-1]) + 2*int(img[i-1][j]) - 2*int(img[i+1][j]) + int(img[i-1][j+1]) - int(img[i+1][j+1])
+    
+    new_image = Gx + Gy 
+    cv2.imshow('Gx', Gx)
+    cv2.imshow('Gy', Gy)
+    cv2.imshow('Sobel', new_image)
+    cv2.waitKey()  
+
+def prewitt(image_path):
+    img = cv2.imread(image_path, 0)
+    rows, cols = img.shape[:2]
+
+    Gx = img.copy()
+    Gy = img.copy()
+
+    cv2.imshow('Original', img)
+
+    for i in range(1, rows-1):
+        for j in range(1, cols-1):
+            Gx[i][j] = -1*int(img[i-1][j-1]) + int(img[i-1][j+1]) - int(img[i][j-1]) + int(img[i][j+1]) - int(img[i+1][j-1]) + int(img[i+1][j+1])
+            Gy[i][j] = -1*int(img[i-1][j-1]) + int(img[i+1][j-1]) - int(img[i-1][j]) + int(img[i+1][j]) - int(img[i-1][j+1]) + int(img[i+1][j+1])
+    
+    new_image = Gx + Gy 
+    cv2.imshow('Gx', Gx)
+    cv2.imshow('Gy', Gy)
+    cv2.imshow('PreWitt', new_image)
+    cv2.waitKey()  
 
 if __name__ == "__main__":
     path = "/home/sergio/TCG/PruebasImagenes/lenaG.png"
-    # path2 = "/home/sergio/TCG/PruebasImagenes/circuit.jpg"
-    path2 = "/home/sergio/TCG/imagenesTCG/coins.png"
-    save = "/home/sergio/TCG/results/result.jpg"
+    path2 = "/home/sergio/TCG/PruebasImagenes/circuit.jpg"
+    # path2 = "/home/sergio/TCG/imagenesTCG/coins.png"
+    # path2 = "/home/sergio/TCG/imagenesTCG/nino.jpg"
+    # save = "/home/sergio/TCG/results/result.jpg"
     
     #HISTOGRAMA
-    # drawHistogram(path2)
+    drawHistogram(path2)
 
     # FILTROS
     # media(path)
+    # media_ponderada(path, 10)
     # mediana(path)
     # maxmin_max(path)
-    # maxmin_min(path)
+    # maxmin_max(path)
     # gauss(path)
 
     #BORDES
     # derivada1(path2)
     # derivada2(path2)
-    laPlace(path2)
+    # laPlace(path2)
+    # roberts(path2)
+    # sobel(path2)
+    # prewitt(path2)
 
 
 
